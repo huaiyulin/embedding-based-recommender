178c29a40ef066e7ccdb30c73bef1c3576ce5c52
Huaiyu <huaiyulin1026@gmail.com>
2019-04-04 20:19:53 +0800

    finish negative_sampling, turn news_ids to vectors

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,19 +3,24 @@ import pickle
 import datetime
 import logging
 import numpy as np
+import random
 import os
 
 data_source_dir = '../data'
 output_temp_dir = os.path.join(data_source_dir,'mid-product')
-user_to_news_list_path = os.path.join(output_temp_dir, 'user_to_news_list.pkl')
-recommend_pool_path  = os.path.join(output_temp_dir, 'recommend_pool.pkl')
-
+if not os.path.exists(output_temp_dir):
+        os.makedirs(output_temp_dir)
+        
 news_train_dates = {'start':11,'end':12}
 news_recommend_dates = {'start':11,'end':12}
-
 news_train_paths = [os.path.join(data_source_dir,'event-201810{}.pkl'.format(str(x).zfill(2))) for x in range(news_train_dates['start'], news_train_dates['end'])]
 news_recommend_paths = [os.path.join(data_source_dir,'event-201810{}.pkl'.format(str(x).zfill(2))) for x in range(news_recommend_dates['start'], news_recommend_dates['end'])]
+news_vec_dic_pah = os.path.join(data_source_dir,'news_vec_dic.pkl')
 
+user_to_news_list_path = os.path.join(output_temp_dir, 'user_to_news_list.pkl')
+recommend_pool_path  = os.path.join(output_temp_dir, 'recommend_pool.pkl')
+user_to_news_pos_vec_path = os.path.join(output_temp_dir, 'user_to_news_pos_vec.pkl')
+user_to_news_neg_vec_path = os.path.join(output_temp_dir, 'user_to_news_neg_vec.pkl')
 
 # datasource config
 event_time = 'event_timestamp'
@@ -55,15 +60,12 @@ def build_user_to_news_history():
     df = clean_data(df)
     df.sort_values(by=event_time,ascending=False, inplace=True) # 將點擊事件由新到舊排序
     user_to_news_list = dataframe_to_user_news_dic(df)
-
     logging.info('saving user_to_news_list...')
-
-    if not os.path.exists(output_temp_dir):
-        os.makedirs(output_temp_dir)
 
     with open(user_to_news_list_path, 'wb') as fp:
         pickle.dump(user_to_news_list,fp)
     logging.info('complete saving user_to_news_list.')
+    return user_to_news_list
 
 def build_recommend_pool(top = 5000, at_least = 10):
     logging.info('=== BUILDING === recommend_pool...')
@@ -73,16 +75,51 @@ def build_recommend_pool(top = 5000, at_least = 10):
     recommend_pool = news_count[(news_count.index < top) + (news_count[0] > at_least).values][news_id].tolist()
     logging.info('saving recommend_pool...')
 
-    if not os.path.exists(output_temp_dir):
-        os.makedirs(output_temp_dir)
-
     with open(recommend_pool_path, 'wb') as fp:
         pickle.dump(recommend_pool,fp)
-
     logging.info('complete saving recommend_pool.')
+    return recommend_pool
 
-      
+def convert_news_id_to_vec(news_vec_dic,news_id):
+    if news_id in news_vec_dic:
+        return news_vec_dic[news_id]
+    else:
+        return None
+
+def news_ids_to_vecs(news_list,news_vec_dic,items=-1):
+    news_vecs = np.asarray([news_vec_dic[x] for x in news_list if x in news_vec_dic])
+    if items != -1:
+        news_vecs = news_vecs[:items]
+    return news_vecs
+
+def get_neg_ids_by_pos_ids(pos_ids, sampling_pool):
+    neg_ids = random.sample(sampling_pool, len(pos_ids)*2)
+    neg_ids = [x for x in neg_ids if x not in pos_ids]
+    return neg_ids
+
+def build_vec_pairs_from_history(user_news_dic,sampling_pool,news_vec_dic):
+    logging.info('=== BUILDING === user_news_vec_pairs...')
+    user_to_news_pos_vec = {}
+    user_to_news_neg_vec = {}
+    for user_id, pos_ids in user_news_dic.items():
+        neg_ids = get_neg_ids_by_pos_ids(pos_ids,sampling_pool)
+        pos_vecs = news_ids_to_vecs(pos_ids, news_vec_dic)
+        neg_vecs = news_ids_to_vecs(neg_ids, news_vec_dic, items=len(pos_vecs))
+        user_to_news_pos_vec[user_id] = pos_vecs
+        user_to_news_neg_vec[user_id] = neg_vecs
+    with open(user_to_news_pos_vec_path, 'wb') as fp:
+        pickle.dump(user_to_news_pos_vec,fp)
+    logging.info('complete saving user_to_news_pos_vec.')
+    with open(user_to_news_neg_vec_path, 'wb') as fp:
+        pickle.dump(user_to_news_neg_vec,fp)
+    logging.info('complete saving user_to_news_neg_vec.')
+
+    return user_to_news_pos_vec, user_to_news_neg_vec
+
+
 if __name__ == '__main__':
     logging_setup()
-    build_user_to_news_history()
-    build_recommend_pool()
+    user_news_dic = build_user_to_news_history()
+    sampling_pool = build_recommend_pool()
+    news_vec_dic  = pd.read_pickle(news_vec_dic_pah)
+    user_to_news_pos_vec, user_to_news_neg_vec = build_vec_pairs_from_history(user_news_dic,sampling_pool,news_vec_dic)
