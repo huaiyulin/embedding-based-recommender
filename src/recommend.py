88170edf2f337a37cc4a8feb3f4b8aadb245b3ba
Huaiyu <huaiyulin1026@gmail.com>
2019-05-04 22:13:27 +0800

    add recommend.py

---
+++ src/recommend.py
@@ -0,0 +1,63 @@
+import logging
+import os
+from preprocessor import Preprocessor
+from user_model import UserModel
+from annoy import AnnoyIndex
+from recommender import Recommender
+import random
+import pandas as pd
+import random
+
+data_source_dir = '../data' 
+output_name = '0428'
+
+def logging_setup():
+    logging.basicConfig(
+        format='%(asctime)s | %(levelname)s | %(name)s: %(message)s',
+        level=logging.INFO, datefmt='%m-%d %H:%M:%S'
+    )
+    
+
+if __name__ == '__main__':
+	logging_setup()
+	logging.info('=============== Lauch Recommender ===============')
+
+	recommender = Recommender(dir=data_source_dir,name=output_name)
+	recommender.load_vec_pool()
+	
+
+	while True:
+		inputValue = input("user_id,news_id,realtime(y/n),items: ")
+		
+		if inputValue == 'stop':
+			break
+
+		if inputValue == 'demo':
+			user_id, user_vec = random.choice(list(recommender.user_vec_pool.items()))
+			news_id, news_vec = random.choice(list(recommender.news_vec_pool.items()))
+			r_list_u = recommender.get_ranking_list_by_both(user_id,news_id, True, 10)
+			print("user_id:{u_id}".format(u_id=user_id))
+			print("news_id:{n_id}".format(n_id=news_id))
+			print(r_list_u)
+			continue
+		
+		if len(inputValue.split(",")) != 4:
+			print('Please input exact 4 values and separate them by comma.')
+			continue
+		
+		## input processing
+
+		user_id, news_id, realtime, items = inputValue.split(",")	
+		
+		if realtime == 'n':
+			realtime = False
+	
+		try:
+			items = int(items)
+		except:
+			items = 10
+
+		## return rec_list
+		r_list_u = recommender.get_ranking_list_by_both(user_id,news_id, realtime, items)
+		print(r_list_u)
+		
